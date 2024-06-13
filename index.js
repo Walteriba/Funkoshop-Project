0b1e94c68aaf2daff568be0ca6e786436519474b
Walteriba <walter_ibarrola@hotmail.com.ar>
2024-06-13 10:59:47 -0300

    buscando la config de vercel

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set("trust proxy", 1);
 // Configurar express-session
 app.use(
   session({
-    secret: "keyboard cat",
+    secret: "keyboardcat",
     resave: false,
     saveUninitialized: true,
     proxy: true,
@@ -20,7 +20,7 @@ app.use(
       secure: true,
       maxAge: 1000 * 60 * 60 * 48,
       sameSite: "none",
-      domain: '.domain.com'
+      domain: '.funkoshop-project.vercel.app'
     },
   })
 );
