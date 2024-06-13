d57b623bd8b7bf70f9aaaec0c1da1b2874982165
Walteriba <walter_ibarrola@hotmail.com.ar>
2024-06-13 12:54:15 -0300

    buscando la config de vercel

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
       secure: true,
       maxAge: 1000 * 60 * 60 * 48,
       sameSite: "none",
-      domain: 'funkoshop-project.vercel.app'
+      domain: '.funkoshop-project.vercel.app'
     },
   })
 );
